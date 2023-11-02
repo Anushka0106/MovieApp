53d7bbbdedc8dde45f50f526aa8ecc3411faabff
anushka0106 <anushkash01062003@gmail.com>
2023-11-02 10:45:21 +0530

    movie website application

--- src/Context.js
+++ src/Context.js
@@ -8,7 +8,7 @@
 // consumer(person)
 import React, { useContext, useEffect, useState } from "react";
 
-const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 const AppContext = React.createContext();
 // we need to create a provider function
 const AppProvider = ({ children }) => {
@@ -17,6 +17,7 @@ const AppProvider = ({ children }) => {
     const [isError,setIsError]=useState({show:"false",msg:""})
     const [query,setQuery]=useState("titanic");
   const getMovies = async (url) => {
+    setIsLoading(true)
     try {
       const res = await fetch(url);
       const data = await res.json();
--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,33 +1,73 @@
-import { NavLink, useParams } from "react-router-dom";
-import useFetch from "./useFetch";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { API_URL } from './Context';
+import { NavLink } from 'react-router-dom';
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const { isLoading, movie, isError } = useFetch(`&i=${id}`);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [movie, setMovie] = useState({});
+
+  const getMovie = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.Response === 'True') {
+        setIsLoading(false);
+        setMovie(data);
+        setIsError(false);
+      } else {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      getMovie(`${API_URL}&i=${id}`);
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [id]);
 
   if (isLoading) {
     return (
-      <section className="movie-section">
-        <div className="loading">Loading....</div>
-      </section>
+      <div className='movie-section'>
+        <div className='loading'>Loading...</div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className='movie-section'>
+        <div className='error'>An error occurred while fetching the movie details.</div>
+      </div>
     );
   }
 
   return (
-    <section className="movie-section">
-      <div className="movie-card">
+    <section className='movie-section'>
+      <div className='movie-card'>
         <figure>
-          <img src={movie.Poster} alt={movie.Title} />
+          <img src={movie.Poster} alt='' />
         </figure>
-        <div className="card-content">
-          <p className="title">{movie.Title}</p>
-          <p className="card-text">Released: {movie.Released}</p>
-          <p className="card-text">Genre: {movie.Genre}</p>
-          <p className="card-text">IMDb Rating: {movie.imdbRating} / 10</p>
-          <p className="card-text">Country: {movie.Country}</p>
-          <NavLink to="/" className="back-btn">
-            Go Back
-          </NavLink>
+        <div className='card-content'>
+        <p className='title'>{movie.Title} </p>
+        
+        <p className='year'>{movie.Released} </p>
+        <p className='card-text'>{movie.Genre}</p>
+        <p className='card-text'>{movie.imdbRating}/10</p>
+        <p className='card-text'>{movie.Country}</p>
+        <NavLink to="/"  className=".back-btn">Go Back</NavLink>
         </div>
       </div>
     </section>
